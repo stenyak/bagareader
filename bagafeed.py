ef23ba21107dc5bb65b4b3cbf3b5ad9f53443c9d
Bruno Gonzalez <stenyak@stenyak.com>
2010-06-04 13:51:40 +0200

    Better handling of errors. Usable from command line with parameters.

--- bagafeed.py
+++ bagafeed.py
@@ -6,15 +6,15 @@ import locale
 import htmllib
 import htmlrender
 
-def unescapeHtml(text):
+def unescapeHtml(htmlText):
     p = htmllib.HTMLParser(None)
     p.save_bgn()
-    p.feed(text)
+    p.feed(htmlText)
     return p.save_end()
 
-def rmHtmlTags(text):
+def rmHtmlTags(htmlText):
     p = re.compile(r'<.*?>')
-    return p.sub('', text)
+    return p.sub('', htmlText)
 
 def htmlToSimplePlainText(htmlText):
     result = rmHtmlTags(htmlText)
@@ -26,7 +26,7 @@ def htmlToNicePlainText(htmlText):
     return result
 
 def printEntry(entry):
-        print "/-----------------------------------\\"
+        print "/-----------------------------------"
         title = ""
         content = ""
 
@@ -48,24 +48,90 @@ def printEntry(entry):
 
         print title
         print content
-        print "\____________________________________/"
+        print "\____________________________________"
     
-def getEntries(url):
+
+def getFeed(url):
+    result = None
     feed = feedparser.parse(url)
-    return feed.entries
+    if feed:
+        statusText = " >>> Feed reply: %r" %feed.status
+        if feed.status == 200:
+            result = feed
+            #all OK, continue
+            print statusText
+        elif feed.status == 301:
+            statusText += " (feed permanently moved to %r)" %(feed.href)
+            print statusText
+            result = getFeed(feed.href)
+        elif feed.status == 302:
+            statusText += " (feed temporarily moved)"
+            print statusText
+            result = getFeed(feed.href)
+        elif feed.status == 404:
+            statusText += " (feed not found)"
+            print statusText
+        elif feed.status == 410:
+            statusText += " (feed permanently removed)"
+            print statusText
+        else:
+            statusText += " (unkown status %r)" %feed.status
+            print statusText
+    else:
+        print "Got no feed."
+    return result
+
+def getEntries(url):
+    feed = getFeed(url)
+    if feed:
+        return feed.entries
+    else:
+        print "Couldn't get feed."
 
 def printFeed(url):
-    for entry in getEntries(url):
-        printEntry (entry)
-        print ""
+    entries = getEntries(url)
+    if entries:
+        if len(entries) > 0:
+            for entry in entries:
+                printEntry (entry)
+                print ""
+        else:
+            print "Empty feed."
+    else:
+        print "Got no feed."
 
 def printLastEntry(url):
-    printEntry (getEntries(url)[-1])
+    entries = getEntries(url)
+    if entries:
+        if len(entries) > 0:
+            printEntry (entries[-1])
+        else:
+            print "Empty feed."
 
 
-#printFeed ("http://www.motorpasion.com/index.xml")
-printLastEntry ("http://www.elcorreodigital.com/vizcaya/rss/feeds/vizcaya.xml")
+if __name__ == '__main__':
+    import sys
+    args = sys.argv[1:]
+    app = sys.argv[0]
 
+    type = ""
+    url = ""
+
+    if len(args) > 1:
+        type = args[0]
+        url =  args[1]
+
+        if type == "all":
+            printFeed(url)
+        if type == "last":
+            printLastEntry(url)
+    else:
+        print "%r: missing parameters" %app
+        print "Usage: %r <all | last> URL" %app
+        print ""
+        print "E.g. %r last http://www.motorpasion.com/index.xml" %app
+        print "E.g. %r all http://www.elcorreodigital.com/vizcaya/rss/feeds/vizcaya.xml" %app
+    
 """
 ---------------
 ------'author':	''
